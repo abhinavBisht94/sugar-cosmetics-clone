4eed521f885e96f22e41c22cc5354647013906c0
Akshay Patil <53942517+AkshayPatil96@users.noreply.github.com>
2022-06-14 23:18:53 +0530

    added footer file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import db from "./API/db.json";
 import { Products } from "./pages/Products";
 import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       {/* <Products data={db}></Products> */}
-      <Navbar />
+      {/* <Navbar /> */}
+      <Footer />
     </div>
   );
 }
