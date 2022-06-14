f56da1793fd9cf1faab73ee19ae0b4a415cdf9e4
P Naga <leadernaga90@gmail.com>
2022-06-14 22:41:27 +0530

    schema and model is added

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,70 @@
-const express = require("express");
-const app = express();
-require("dotenv").config();
+const express = require('express')
 
-const mongoose = require("mongoose");
-const authRouter = require("./routes/auth/auth");
-const cartRouter = require("./routes/Cart/cart");
-const paymentRouter = require("./routes/Payment/payment");
-const productRouter = require("./routes/Product/product");
+const cors = require('cors')
+require('dotenv').config()
+
+const mongoose = require('mongoose')
+const authRouter = require('./routes/auth/auth')
+const cartRouter = require('./routes/Cart/cart')
+const paymentRouter = require('./routes/Payment/payment')
+const productRouter = require('./routes/Product/product')
+
+const app = express()
 const PORT = process.env.PORT || 8080;
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors())
 
-const connection = mongoose.connect(process.env.database, {
-  useNewUrlParser: true,
-});
+const connection = mongoose.connect(process.env.database)
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const productSchema = new mongoose.Schema({
+  prodEyesHeading: String,
+  prodEyesBanner:String,
+  Title: { type: String, required: true },
+  ImageUrl: { type: String, required: true },
+  Rating: { type: Number, required: true },
+  Price: { type: Number, required: true },
+  category: { type: String, required: true },
+  Currency: { type: String, enum: ['$', '₹'] },
 
-app.use("/product", productRouter);
-app.use("/auth", authRouter);
-app.use("/cart", cartRouter);
-app.use("/payment", paymentRouter);
+  types: [
+    {
+      title: String,
+      image: String,
+      secondary_title: String,
+    },
+  ],
+})
+
+const Product = mongoose.model('product', productSchema)
+console.log(Product, 'index product model')
+
+
+
+// app.post('/product/new', async (req, res) => {
+//   console.log('it is coming')
+//   const body = req.body
+//   console.log(body)
+//   const product = await Product(body)
+//   product.save((error, data) => {
+//     if (error) console.log(error)
+//     res.send(data)
+//   })
+// })
+
+app.use('/product', productRouter)
+app.use('/auth', authRouter)
+app.use('/cart', cartRouter)
+app.use('/payment', paymentRouter)
 
 app.listen(PORT, async () => {
   try {
-    await connection;
+    await connection
+    console.log('data base connected')
   } catch (error) {
-    console.log("error: ", error);
+    console.log('error: ', error)
   }
-  console.log("Server  Starts at http://localhost:8080/");
-});
+  console.log('Server  Starts at http://localhost:8080/')
+})
+
+module.exports = { Product }
--- backend/routes/Product/product.js
+++ backend/routes/Product/product.js
@@ -1,5 +1,25 @@
-const { Router } = require("express");
+const { Router } = require('express')
 
-const productRouter = Router();
+const productRouter = Router()
+const {Product} = require('../../index')
 
-module.exports = productRouter;
+console.log('came')
+
+productRouter.get('/new', (req, res) => {
+  res.send('working')
+})
+
+productRouter.post('/new', async (req, res) => {
+  console.log('it is coming')
+  // const body = req.body
+  // console.log(body)
+  // console.log(Product, 'model')
+  const product = await Product(req.body)
+
+  product.save((err, data) => {
+    if (err) res.send(err)
+    res.send(data)
+  })
+})
+
+module.exports = productRouter
