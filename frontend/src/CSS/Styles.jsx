612b9077b3765a8182ed743ecde6ffb240b88c5d
Chiranjit Behera <99542444+Chiranjit34@users.noreply.github.com>
2022-06-17 13:35:46 +0530

    cart-payment-success

---
+++ frontend/src/CSS/Styles.jsx
@@ -0,0 +1,153 @@
+import styled from "styled-components";
+
+export const Body = styled.div`
+  background-color: #f5f5f5;
+`;
+
+export const Makeup = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-left: 10px;
+  padding-right: 10px;
+`;
+
+export const MakeupL = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 8%;
+`;
+
+export const CartDiv = styled.div`
+  display: flex;
+  width: 95%;
+  height: auto;
+  margin: 20px;
+  border-radius: 15px;
+  background-color: white;
+  padding: 20px;
+  gap: 20px;
+`;
+
+export const CartDiv1 = styled.div`
+  width: 50%;
+  background-color: #f5f5f5;
+  border-radius: 15px;
+`;
+
+export const CartDiv2 = styled.div`
+  width: 50%;
+  padding-left: 25px;
+  background-color: #f5f5f5;
+  border-radius: 15px;
+  padding-bottom: 20px;
+  padding-right: 20px;
+`;
+
+export const OrderSummary = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+`;
+
+export const PriceDis = styled.div`
+  background-color: white;
+  width: 90%;
+  margin-top: 10px;
+  color: gray;
+  padding-left: 18px;
+`;
+
+export const DelInfoBTN = styled.button`
+  width: 325px;
+  height: 32.5px;
+  background-color: black;
+  color: white;
+  border: none;
+  cursor: pointer;
+`;
+
+export const DelInfoBTN1 = styled.button`
+  width: 325px;
+  height: 32.5px;
+  background-color: black;
+  color: white;
+  border: none;
+  cursor: pointer;
+`;
+
+export const DelInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 70%;
+  height: 32px;
+  border: 1px solid;
+  margin-top: 20px;
+  align-items: center;
+`;
+
+export const Dis1 = styled.div`
+  display: flex;
+`;
+
+export const Dis11 = styled.div`
+  display: flex;
+  width: 25%;
+  gap: 10px;
+  margin-bottom: 10px;
+`;
+
+export const Dis2 = styled.div`
+  display: flex;
+`;
+
+export const Dis21 = styled.div`
+  display: flex;
+  width: 25%;
+  gap: 10px;
+  margin-bottom: 10px;
+`;
+
+export const Dis3 = styled.div`
+  display: flex;
+`;
+
+export const Dis31 = styled.div`
+  display: flex;
+  width: 25%;
+  gap: 10px;
+  margin-bottom: 10px;
+`;
+
+export const Dis4 = styled.div`
+  display: flex;
+`;
+
+export const Dis41 = styled.div`
+  display: flex;
+  width: 25%;
+  gap: 10px;
+  margin-bottom: 10px;
+`;
+
+export const Payable = styled.div`
+  color: black;
+  font-weight: bold;
+`;
+export const PayMethods = styled.div`
+  height: auto;
+`;
+export const PayMethods1 = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+  border: 1px solid gray;
+  margin-top: 10px;
+  cursor: pointer;
+`;
+export const Pm1 = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 5px;
+`;
---
+++ frontend/src/CSS/SuccessStyles.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Box = styled.div`
+  text-align: center;
+  font-family: roboto, sans-serif;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  padding: 30px;
+  width: fit-content;
+  margin: 60px auto;
+  text-align: center;
+  border-radius: 10px;
+`;
+
+export const Image = styled.img`
+  width: 100px;
+  margin-top: 8%;
+`;
+
+export const Button = styled.button`
+  background-color: green;
+  color: white;
+  padding: 5px;
+  font-size: 16px;
+  border-radius: 6px;
+`;
